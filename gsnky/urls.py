7522ff0e01eff1d0795796188a41433ab4131f61
yoseng28 <yoseng@163.com>
2018-06-25 17:52:00 +0800

    修改静态页面跳转地址

--- gsnky/urls.py
+++ gsnky/urls.py
@@ -15,21 +15,30 @@ Including another URLconf
 """
 import xadmin
 from django.conf.urls import url
-from django.contrib import admin
 from django.urls import path, re_path, include
 from django.views.generic import TemplateView
 from django.views.static import serve
 
 from gsnky.settings import MEDIA_ROOT
+from products.views import productsTreeView
 from zb.views import ZBView
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
     path('admin/', xadmin.site.urls),
+
     # 上传文件的访问配置
     re_path(r'^media/(?P<path>.*)$', serve, {'document_root': MEDIA_ROOT}),
-    path('', TemplateView.as_view(template_name='index.html'), name='index'),
-
-    re_path(r'^zb/(?P<product_id>.*)/$', ZBView.as_view(), name='zb_view'),
+    # Plugins
     url(r'^ueditor/', include('DjangoUeditor.urls')),
+
+    # 静态页面跳转
+    path('', TemplateView.as_view(template_name='index.html'), name='index'),
+    path('contact/', TemplateView.as_view(template_name='contact.html'), name='contact'),
+    path('baidumap/', TemplateView.as_view(template_name='map.html'), name='baidumap'),
+
+    # 指标树显示
+    path('zblist/', productsTreeView, name='zblist'),
+    # 指标详情页
+    re_path(r'^zblist/(?P<product_id>.*)/$', ZBView.as_view(), name='zb_view'),
+
 ]
