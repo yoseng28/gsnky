44071ac72cc5bc6260b0c79022ad5fc64d2680a2
yoseng28 <yoseng@163.com>
2018-06-25 18:03:01 +0800

    修改指标页面

--- apps/products/views.py
+++ apps/products/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+from products.models import ProductType, Product
+
+
+def productsTreeView(request):
+    product_list = Product.objects.all()
+    product_type_nodes = ProductType.objects.all()
+    return render(request, 'zblist.html', {'nodes': product_type_nodes, 'product_list': product_list})
--- apps/zb/views.py
+++ apps/zb/views.py
@@ -1,20 +1,60 @@
 from django.shortcuts import render
 from django.views.generic.base import View
 
-from zb.models import ZBList, GGZB, CDHJ
+from products.models import Product
+from zb.models import GGZB, CDHJ, JBYYZB, DNATP, TYXYYZB
 
 
 class ZBView(View):
     def get(self, request, product_id):
-        list_ggzb_avg = []
-        list_ggzb_range = []
-        query_set_avg = GGZB.objects.select_related('zb_list').filter(product_id=product_id, avg_range=1)
-        query_set_range = GGZB.objects.select_related('zb_list').filter(product_id=product_id, avg_range=2)
-        for qs in query_set_avg:
-            list_ggzb_avg.append(qs)
-        for qs in query_set_range:
-            list_ggzb_range.append(qs)
-        return render(request, 'table.html', {
-            'ggzb_avg': list_ggzb_avg,
-            'ggzb_range': list_ggzb_range,
+
+        product_ = Product.objects.get(id=product_id)
+        if product_ is None:
+            return render(request, 'zbinfo.html', {})
+
+        # 指标-平均值
+        query_ggzb_avg = GGZB.objects.select_related('zb_list').filter(product_id=product_id, avg_range=1) \
+            .order_by('zb_list_id')
+        query_jbyyzb_avg = JBYYZB.objects.select_related('zb_list').filter(product_id=product_id, avg_range=1) \
+            .order_by('zb_list_id')
+        query_tyxyyzb_avg = TYXYYZB.objects.select_related('zb_list').filter(product_id=product_id, avg_range=1) \
+            .order_by('zb_list_id')
+        query_dnatp_avg = DNATP.objects.select_related('zb_list').filter(product_id=product_id, avg_range=1) \
+            .order_by('zb_list_id')
+        query_cdhj_avg = CDHJ.objects.select_related('zb_list').filter(product_id=product_id, avg_range=1) \
+            .order_by('zb_list_id')
+
+        # 指标-范围
+        query_ggzb_range = GGZB.objects.select_related('zb_list').filter(product_id=product_id, avg_range=2) \
+            .order_by('zb_list_id')
+        query_jbyyzb_range = JBYYZB.objects.select_related('zb_list').filter(product_id=product_id, avg_range=2) \
+            .order_by('zb_list_id')
+        query_tyxyyzb_range = TYXYYZB.objects.select_related('zb_list').filter(product_id=product_id, avg_range=2) \
+            .order_by('zb_list_id')
+        query_dnatp_range = DNATP.objects.select_related('zb_list').filter(product_id=product_id, avg_range=2) \
+            .order_by('zb_list_id')
+        query_cdhj_range = CDHJ.objects.select_related('zb_list').filter(product_id=product_id, avg_range=2) \
+            .order_by('zb_list_id')
+
+        return render(request, 'zbinfo.html', {
+            'product_name': product_.name,
+            # 平均值
+            'ggzb_avg': ZBView.build_list_zb(query_ggzb_avg),
+            'jbyyzb_avg': ZBView.build_list_zb(query_jbyyzb_avg),
+            'tyxyyzb_avg': ZBView.build_list_zb(query_tyxyyzb_avg),
+            'dnatp_avg': ZBView.build_list_zb(query_dnatp_avg),
+            'cdhj_avg': ZBView.build_list_zb(query_cdhj_avg),
+            # 范围
+            'ggzb_range': ZBView.build_list_zb(query_ggzb_range),
+            'jbyyzb_range': ZBView.build_list_zb(query_jbyyzb_range),
+            'tyxyyzb_range': ZBView.build_list_zb(query_tyxyyzb_range),
+            'dnatp_range': ZBView.build_list_zb(query_dnatp_range),
+            'cdhj_range': ZBView.build_list_zb(query_cdhj_range),
         })
+
+    @staticmethod
+    def build_list_zb(query_set_zb):
+        temp = []
+        for qs in query_set_zb:
+            temp.append(qs)
+        return temp
